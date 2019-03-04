0b4591d0d6302b1574c8444a51e4d6322c73106b
Luca Gonzalez <lucaclementegonzalez@gmail.com>
2019-03-04 22:35:02 +0100

    Borrando el archivo 2 de máster

--- 2.sh
+++
@@ -1,8 +0,0 @@
-echo "Por favor, inserte un número más grande que 0"
-select option in `seq 0 10`;do
-    if [ expr ${option} % 2 -eq 0 ];then
-	echo "El valor es par"
-    else
-	echo "El valor es impar"
-    fi
-done
